a92f09e710f9985f89208457745f96324e0d8f98
TRVevel <piscionequentin@gmail.com>
2024-12-26 15:05:31 +0100

    init branch Quentin

---
+++ src/app/gestion-commandes/gestion-commandes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-commandes',
+  imports: [],
+  templateUrl: './gestion-commandes.component.html',
+  styleUrl: './gestion-commandes.component.css'
+})
+export class GestionCommandesComponent {
+
+}

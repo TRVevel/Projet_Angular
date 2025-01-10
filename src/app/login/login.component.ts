5321b49cea8045556e4c8a1d17e20e28a4329aa1
Mohammed Sahli <mohammed.sahli@outlook.com>
2025-01-10 09:47:50 +0100

    login.ts récupère le token : done

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,20 +1,19 @@
 import { Component } from '@angular/core';
 import { FormsModule} from '@angular/forms';
 import { Router } from '@angular/router';
-import { DashboardComponent } from '../dashboard/dashboard.component';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-login',
   imports:[FormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
-  
 })
 export class LoginComponent {
   username: string = '';
   password: string = '';
   
-  constructor(private router: Router) { }
+  constructor(private router: Router,private httpservice:HttpService) { }
 
   onSubmit(): void {
     // on ajoute ici la logique pour l'authentification, par exemple une API
@@ -22,9 +21,14 @@ export class LoginComponent {
       console.log('Utilisateur:', this.username);
       console.log('Mot de passe:', this.password);
       this.router.navigate(["/dashboard"])
-   
-    } else {
-      console.log('Veuillez renseigner tous les champs.');
-    }
+      let authBody={"username":"admin","password":"pwd"}
+
+      this.httpservice.login(authBody).subscribe(value=>{
+        console.log(value)
+        localStorage.setItem("token", value.token)
+        })
+      }
+    else { console.log('Veuillez renseigner tous les champs.'); }
   }
 }
+

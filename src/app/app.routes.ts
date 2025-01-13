c3b1fb0e008c388aefdcfd0a575c1cb4be399261
TRVevel <piscionequentin@gmail.com>
2025-01-13 12:25:51 +0100

    postNettoyage début des formulaires

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,17 @@
 import { Routes } from '@angular/router';
-import { GestionCommandesComponent } from './gestion-commandes/gestion-commandes.component';
+import { GestionCommandesComponent } from './Commandes/gestion-commandes/gestion-commandes.component';
+import { FormulaireComponent } from './Commandes/formulaire/formulaire.component';
 
 export const routes: Routes = [
     {
         path:'commandes',
         component: GestionCommandesComponent,
+        pathMatch: 'full'
+
+    },
+    {
+        path:'formulaire',
+        component: FormulaireComponent,
         pathMatch: 'full'
 
     },
